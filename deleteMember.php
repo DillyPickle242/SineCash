80559f858b1009afdf87a55ed4d83e199134cd55
Dillon Harrington <peeps242dillon@gmail.com>
2022-11-26 15:10:40 -0500

    ability to delete people from family as adult

--- deleteMember.php
+++ deleteMember.php
@@ -24,8 +24,10 @@
         $username = getNameFromId($_POST['id'])['username'];
 
         print_r("
-        <div class='Etext'>Are you SURE you want to delete '$username' from your family? This action CANNOT be undone.</div>
-        <button> </button>
+        <div class='Etext'>Are you SURE you want to delete '$username' from your family? This action CANNOT be undone. All transaction history including $username will also be deleted.</div>
+        <form action='deletingMember.php' method='post'>
+        <button name='deletedMember' value='$_POST[id]' class='Etext' id='memberDeleteButton'>Yes, Delete $username from my family</button>
+        </form>
         ")
         
         ?>
---
+++ deletingMember.php
@@ -0,0 +1,36 @@
+<?php
+include 'db.php';
+include 'parentCheck.php';
+if ($parent = 1){
+    //delete person
+$stmt = $db->prepare("DELETE FROM people WHERE `people`.`ID` = ?;");
+$stmt->bind_param("i", $id);
+$id = $_POST['deletedMember'];
+if (!$stmt->execute()) {
+    print("error: " . $db->error);
+}
+//delete their transactions
+$stmt = $db->prepare("DELETE FROM transactionhistory WHERE `transactionhistory`.`sender` = ?;");
+$stmt->bind_param("i", $id);
+$id = $_POST['deletedMember'];
+if (!$stmt->execute()) {
+    print("error: " . $db->error);
+}
+$stmt = $db->prepare("DELETE FROM transactionhistory WHERE `transactionhistory`.`recipient` = ?;");
+$stmt->bind_param("i", $id);
+$id = $_POST['deletedMember'];
+if (!$stmt->execute()) {
+    print("error: " . $db->error);
+}
+//delete their allowance
+$stmt = $db->prepare("DELETE FROM allowance WHERE `allowance`.`ID` = ?;");
+$stmt->bind_param("i", $id);
+$id = $_POST['deletedMember'];
+if (!$stmt->execute()) {
+    print("error: " . $db->error);
+}
+header("location: profilePage.php");
+
+} else {
+    print_r("You aren't a parent. You cannot delete people.");
+}
