05048c98eaf66cb01c519c298e089b224b7119ea
Dillon Harrington <peeps242dillon@gmail.com>
2022-12-08 20:35:14 -0500

    locked icon when family is locked

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
+
 <head>
     <link rel="stylesheet" href="moneyApp.css?<?php echo time(); ?>">
 
-    <?php 
+    <?php
     include_once 'themes.php';
     include_once 'sessionStart.php';
     include_once 'db.php';
@@ -10,40 +11,41 @@
     include_once 'parentCheck.php';
 
     giveAllowances();
-    
+
     $familyCode = getFamilyCode();
     if (!$familyCode) {
         header("location: loginPage.php");
     }
 
-    
+
     ?>
 </head>
+
 <body>
     <header>
         <div id="topBar">
-                <a href="settingsPage.php">
-                    <img src="images/settings icon.png" id="settingsButton" class="topMenuItems">
-                </a>
-                <a href="profilePage.php">
-                    <img src="images/familyIcon.png" id="profileButton" class="topMenuItems">
-                </a>
-                <a href="index.php">
-                    <div id="title" class="Atext"> SineCash </div>
-                </a>
-                
-                <?php 
-                //crazy sinewaves:
-                // for ($x = 0; $x <=23; $x+=1) {
-                //     print_r("<img src='animated_favicon.gif' class='sine'>");
-                // }
-                ?>
-                
-                
+            <a href="settingsPage.php">
+                <img src="images/settings icon.png" id="settingsButton" class="topMenuItems">
+            </a>
+            <a href="profilePage.php">
+                <img src="images/familyIcon.png" id="profileButton" class="topMenuItems">
+            </a>
+            <a href="index.php">
+                <div id="title" class="Atext"> SineCash </div>
+            </a>
+
+            <?php
+            //crazy sinewaves:
+            // for ($x = 0; $x <=23; $x+=1) {
+            //     print_r("<img src='animated_favicon.gif' class='sine'>");
+            // }
+            ?>
+
+
         </div>
     </header>
     <?php
-    if(isset($_GET['transaction'])){
+    if (isset($_GET['transaction'])) {
         print_r("
         <div id='topMessage' class='topMessageShown'>
             Your transaction was succesfully completed!
@@ -52,106 +54,128 @@
     }
     ?>
     <div id="cashTotalContainer">
-        <?php 
+        <?php
         //Custom Image
 
         $sql = "SELECT backgroundImg FROM people WHERE ID=?";
-                $stmt = $db->prepare($sql);
-                if (!$stmt){
-                    print("error: ".$db->error);
-                }
-                $stmt->bind_param("i", $id);
-                $id = $_SESSION['id'];
-
-                if (!$stmt->execute()) {
-                    print("execute error: ".$db->error);
-                }
-                $result = $stmt->get_result();
-                $imgRow = $result->fetch_assoc(); //row = totalCash array
-                $backgroundFile = $imgRow['backgroundImg'];
-
-        print_r("<img id='cashBackground' src='images\-$backgroundFile.gif'>") 
-        
-        ?>
-        <?php 
-                $sql = "SELECT parent FROM people WHERE ID=?";
-                $stmt = $db->prepare($sql);
-                if (!$stmt){
-                    print("error: ".$db->error);
-                }
-                $stmt->bind_param("i", $id);
-                $id = $_SESSION['id'];
-
-                if (!$stmt->execute()) {
-                    print("execute error: ".$db->error);
-                }
-                $result = $stmt->get_result();
-                $parentRow = $result->fetch_assoc(); //row = totalCash array
-                $parent = $parentRow['parent'];
-
-
-                if ($parent == '1'){
-                    $stmt = $db->prepare("UPDATE `people` SET `totalCash` = 1000000 WHERE `people`.`id` = ?;");
-                    $stmt->bind_param("i", $id);
-
-                    // set parameters and execute
-                    $id = $_SESSION['id'];
-                    if (!$stmt->execute()){
-                        print("error: ".$db->error);
-                    }
-
-                    $sql = "SELECT familyCode FROM people WHERE ID=?";
-                    $stmt = $db->prepare($sql);
-                    if (!$stmt){
-                        print("error: ".$db->error);
-                    }
-                    $stmt->bind_param("i", $id);
-                    $id = $_SESSION['id'];
-
-                    if (!$stmt->execute()) {
-                        print("execute error: ".$db->error);
-                    }
-                    $result = $stmt->get_result();
-                    $familyCodeRow = $result->fetch_assoc();
-                    $familyCode = $familyCodeRow['familyCode']; 
-
-
-                } else {
-                    $sql = "SELECT totalCash FROM people WHERE ID=?";
-                    $stmt = $db->prepare($sql);
-                    if (!$stmt){
-                        print("error: ".$db->error);
-                    }
-                    $stmt->bind_param("i", $id);
-                    $id = $_SESSION['id'];
-
-                    if (!$stmt->execute()) {
-                        print("execute error: ".$db->error);
-                    }
-                    $result = $stmt->get_result();
-                    $cashRow = $result->fetch_assoc(); //row = totalCash array
-                    $totalCash = $cashRow['totalCash']; 
-                }
-                    
-                
-        ?>
-        <p class=Atext id="totalCashDesc"> <?php 
-        if ($parent == '0'){
-            print_r("Your total cash is:");
-        } 
-        if ($parent == '1') {    
-            print_r("You have infinite cash! \r\n Your family code is:");
+        $stmt = $db->prepare($sql);
+        if (!$stmt) {
+            print("error: " . $db->error);
         }
-        ?></p>
-        <p class=Atext id="totalCashNum"> <?php 
-        if ($parent == '0'){
-            print_r("$$totalCash");
-        } 
-        if ($parent == '1') {    
-            print_r("$familyCode");
+        $stmt->bind_param("i", $id);
+        $id = $_SESSION['id'];
+
+        if (!$stmt->execute()) {
+            print("execute error: " . $db->error);
+        }
+        $result = $stmt->get_result();
+        $imgRow = $result->fetch_assoc(); //row = totalCash array
+        $backgroundFile = $imgRow['backgroundImg'];
+
+        print_r("<img id='cashBackground' src='images\-$backgroundFile.gif'>")
+
+        ?>
+        <?php
+        $sql = "SELECT parent FROM people WHERE ID=?";
+        $stmt = $db->prepare($sql);
+        if (!$stmt) {
+            print("error: " . $db->error);
+        }
+        $stmt->bind_param("i", $id);
+        $id = $_SESSION['id'];
+
+        if (!$stmt->execute()) {
+            print("execute error: " . $db->error);
+        }
+        $result = $stmt->get_result();
+        $parentRow = $result->fetch_assoc(); //row = totalCash array
+        $parent = $parentRow['parent'];
+
+
+        if ($parent == '1') {
+            $stmt = $db->prepare("UPDATE `people` SET `totalCash` = 1000000 WHERE `people`.`id` = ?;");
+            $stmt->bind_param("i", $id);
+
+            // set parameters and execute
+            $id = $_SESSION['id'];
+            if (!$stmt->execute()) {
+                print("error: " . $db->error);
+            }
+
+            $sql = "SELECT familyCode FROM people WHERE ID=?";
+            $stmt = $db->prepare($sql);
+            if (!$stmt) {
+                print("error: " . $db->error);
+            }
+            $stmt->bind_param("i", $id);
+            $id = $_SESSION['id'];
+
+            if (!$stmt->execute()) {
+                print("execute error: " . $db->error);
+            }
+            $result = $stmt->get_result();
+            $familyCodeRow = $result->fetch_assoc();
+            $familyCode = $familyCodeRow['familyCode'];
+
+
+            //getting family code
+            $sql = "SELECT familyCode FROM people WHERE ID = ?";
+            $stmt = $db->prepare($sql);
+
+            $stmt->bind_param("s", $id);
+            $id = $_SESSION['id'];
+            if (!$stmt->execute()) {
+                print("execute error: " . $db->error);
+            }
+            $result = $stmt->get_result();
+            $codeRow = $result->fetch_assoc();
+            $familyCode = $codeRow['familyCode'];
+
+            if (familyLocked($familyCode)['familyLocked'] == 1) {
+                $locked = 'locked';
+            } else {
+                $locked = 'unlocked';
+            }
+        } else {
+            $sql = "SELECT totalCash FROM people WHERE ID=?";
+            $stmt = $db->prepare($sql);
+            if (!$stmt) {
+                print("error: " . $db->error);
+            }
+            $stmt->bind_param("i", $id);
+            $id = $_SESSION['id'];
+
+            if (!$stmt->execute()) {
+                print("execute error: " . $db->error);
+            }
+            $result = $stmt->get_result();
+            $cashRow = $result->fetch_assoc(); //row = totalCash array
+            $totalCash = $cashRow['totalCash'];
         }
 
-        ?> </p>
+
+        ?>
+        <p class=Atext id="totalCashDesc"> <?php
+            if ($parent == '0') {
+                print_r("Your total cash is:");
+            }
+            if ($parent == '1' && $locked == 'unlocked') {
+                print_r("You have infinite cash! <br> Your family code is:");
+            } else if ($parent == '1' && $locked == 'locked'){
+                print_r("You have infinite cash! <br> ");
+            }
+            ?></p>
+        <p class=Atext id="totalCashNum"> <?php
+            if ($parent == '0') {
+                print_r("$$totalCash");
+            }
+            if ($parent == '1' && $locked == 'unlocked') {
+                print_r("$familyCode");
+            } else if ($parent == '1' && $locked == 'locked'){
+                print_r("<img id='lockIcon' src='images/lockIcon.png'>");
+            }
+
+            ?> </p>
     </div>
     <div id="sendReceiveA">
         <a href="sendPage.php">
@@ -164,43 +188,43 @@
             <button class="hsButton" id="historyButton">Transaction History</button>
         </a>
     </div>
-    <div id="requestAlerts"> 
+    <div id="requestAlerts">
         <?php
 
-            $sql = "SELECT recipient, amount, note, ID FROM transactionhistory WHERE sendOrRequest = 'request' AND sender = ? AND fulfilled = 'pending'";
+        $sql = "SELECT recipient, amount, note, ID FROM transactionhistory WHERE sendOrRequest = 'request' AND sender = ? AND fulfilled = 'pending'";
+        $stmt = $db->prepare($sql);
+
+        $stmt->bind_param("i", $id);
+        $id = $_SESSION['id'];
+        if (!$stmt->execute()) {
+            print("execute error: " . $db->error);
+        }
+
+
+
+
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_assoc()) {
+            $recipient = $row['recipient'];
+            $amount = $row['amount'];
+            $note = $row['note'];
+            $transactionId = $row['ID'];
+
+            $sql = "SELECT username FROM people WHERE ID = ?";
             $stmt = $db->prepare($sql);
-            
-            $stmt->bind_param("i", $id);
-            $id = $_SESSION['id'];
-            if (!$stmt->execute()) {
-                print("execute error: ".$db->error);
+            if (!$stmt) {
+                print("error: " . $db->error);
             }
+            $stmt->bind_param("i", $id);
+            $id = $recipient;
+            if (!$stmt->execute()) {
+                print("execute error: " . $db->error);
+            }
+            $result2 = $stmt->get_result();
+            $row2 = $result2->fetch_assoc();
+            $username = $row2['username'];
 
-            
-
-
-            $result = $stmt->get_result();
-            while ($row = $result->fetch_assoc()) {
-                $recipient = $row['recipient'];
-                $amount = $row['amount'];
-                $note = $row['note'];
-                $transactionId = $row['ID'];
-
-                $sql = "SELECT username FROM people WHERE ID = ?";
-                $stmt = $db->prepare($sql);
-                if (!$stmt){
-                    print("error: ".$db->error);
-                }
-                $stmt->bind_param("i", $id);
-                $id = $recipient;
-                if (!$stmt->execute()) {
-                    print("execute error: ".$db->error);
-                }
-                $result2 = $stmt->get_result();
-                $row2 = $result2->fetch_assoc();
-                $username = $row2['username'];
-
-                print_r("
+            print_r("
                 <form action='requestRespond.php' method='post'>
                 <div class='alert'> 
                 <div>$username is requesting $$amount from you for $note. Would you like to accept? </div>
@@ -215,14 +239,15 @@
                 </div>
                 </form>
                 ");
-            }
+        }
 
-                
-        
+
+
         ?>
 
     </div>
 
     <script src="moneyApp.js"></script>
 </body>
+
 </html>
