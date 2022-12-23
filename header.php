66ab3b4329e9ed5627477e3ee18919ec8d03d0a3
Dillon Harrington <peeps242dillon@gmail.com>
2022-12-23 18:05:58 -0500

    fixing menu a little

--- header.php
+++ header.php
@@ -11,12 +11,18 @@
     <img id='exitIcon' src="images/exitIcon.png">
     <div id="menuItemContainer">
         <a href='index.php'><button class='menuItem'>Home Page</button></a><br>
-        <a href='settingsPage.php'><button class='menuItem'>Settings</button></a><br>
-        <a href='profilePage.php'><button class='menuItem'>Edit Family</button></a><br>
-        <a href='sendPage.php'><button class='menuItem'>Send</button></a><br>
-        <a href='requestPage.php'><button class='menuItem'>Request</button></a><br>
-        <a href='transactionHistoryPage.php'><button class='menuItem'>Transaction History</button></a><br>
-        <a href='promiseCards.php'><button class='menuItem'>Promise Cards</button></a><br>
-        <a href='promiseCardCreate.php'><button class='menuItem'>Create a promise card</button></a><br>
+        <div class='menuGroup'>
+            <a href='settingsPage.php'><button class='menuItem'>Settings</button></a><br>
+            <a href='profilePage.php'><button class='menuItem'>Edit Family</button></a><br>
+        </div>
+        <div class='menuGroup'>
+            <a href='sendPage.php'><button class='menuItem'>Send</button></a><br>
+            <a href='requestPage.php'><button class='menuItem'>Request</button></a><br>
+            <a href='transactionHistoryPage.php'><button class='menuItem'>Transaction History</button></a><br>
+        </div>
+        <div class='menuGroup'>
+            <a href='promiseCards.php'><button class='menuItem'>Promise Cards</button></a><br>
+            <a href='promiseCardCreate.php'><button class='menuItem'>Create a promise card</button></a><br>
+        </div>
     </div>
 </div>
